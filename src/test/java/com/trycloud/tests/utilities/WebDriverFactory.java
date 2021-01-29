5675a561d3e6441af5b421fc7062032b3b0b71d5
jiten228 <jiten228@yahoo.com>
2021-01-29 12:14:23 -0600

    Initial commit

---
+++ src/test/java/com/trycloud/tests/utilities/WebDriverFactory.java
@@ -0,0 +1,29 @@
+package com.cybertek.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+
+    //TASK: NEW METHOD CREATION
+    //   Method name: getDriver
+    //   Static method
+    //   Accepts string argument: browserType --> will determine what browser opens
+    //       -> if "chrome" opens chrome browser
+    //       -> if "firefox" opens firefox browser
+    //   return type: WebDriver
+
+    public static WebDriver getDriver(String browserType) {
+        if ("chrome".equalsIgnoreCase(browserType)) {
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        } else if ("firefox".equalsIgnoreCase(browserType)) {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        } else {
+            return null;
+        }
+    }
+}
