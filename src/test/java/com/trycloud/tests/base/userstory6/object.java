9f46bf10fec8f471b8a829efc59ec1d54269dbd4
Ariz <74077376+Ariz023@users.noreply.github.com>
2021-02-03 14:30:10 -0600

    corrected

--- src/test/java/com/trycloud/tests/base/userstory6/object.java
+++ src/test/java/com/trycloud/tests/base/userstory6/object.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -19,7 +20,7 @@ public class object {
     WebDriver driver;
 
     @BeforeMethod
-    public void tryCloudLogin() {
+    public void tryCloudLogin() throws InterruptedException {
         // Open chrome browser
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -27,15 +28,17 @@ public class object {
         driver.get("http://qa.trycloud.net/index.php/login?clear=1");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
     }
 
     /*
     Usernames: User20, User50, User80, User110
-    Password: UserPassword: 123
+    Password: UserPass123
      */
-    @Test
-    public void DataLogin() throws InterruptedException {
 
+    @Test
+    public void NotesModule() throws InterruptedException {
+//1.Login
         String userName = "User20";
         String password = "Userpass123";
         //1.Login as a user
@@ -45,25 +48,16 @@ public class object {
         Thread.sleep(3000);
 
         //2.Click "Notes" module
-      driver.findElement(By.xpath("//a[@href='/index.php/apps/notes/']")).click();
-
+        driver.findElement(By.xpath("//a[@href='/index.php/apps/notes/']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"notes_new_note\"]")).click();
         //3.Verify the page title is "Notes" modules title.
         System.out.println(driver.getTitle());
+//write "Note"
+        driver.findElement(By.xpath("//*[@id=\"note-container\"]/div/div[2]/div/div[1]/div[6]")).sendKeys("Note #1");
+//
     }
 
-
-    @Test
-    public void sendNotes(){
-
-
-
-
-
-
-
-
-
-
-    }
 
 }
+
