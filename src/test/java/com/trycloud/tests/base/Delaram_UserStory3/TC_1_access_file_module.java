6dace784739b951fb000d9e788da929d5e5db0da
delaram1978 <delaram78a@gmail.com>
2021-02-07 17:03:16 -0600

    finished 1

--- src/test/java/com/trycloud/tests/base/Delaram_UserStory3/TC_1_access_file_module.java
+++ src/test/java/com/trycloud/tests/base/Delaram_UserStory3/TC_1_access_file_module.java
@@ -3,6 +3,8 @@ package com.trycloud.tests.base.Delaram_UserStory3;
 import com.trycloud.tests.base.TestBase;
 import com.trycloud.tests.utilities.ConfigurationReader;
 import com.trycloud.tests.utilities.LoginUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class TC_1_access_file_module extends TestBase {
@@ -13,6 +15,9 @@ public class TC_1_access_file_module extends TestBase {
 
      String username = ConfigurationReader.getProperty("username2");
         LoginUtils.loginToTryCloud(driver, username);
+
+        WebElement file_module = driver.findElement(By.xpath("(//li[@data-id='files' ])[1]"));
+        file_module.click();
 
 
 
