1a4ede37504b7e2aaeae9c5aa7b00f4074efe301
Ariz <74077376+Ariz023@users.noreply.github.com>
2021-02-09 16:11:28 -0600

    added new method

--- src/test/java/com/trycloud/tests/base/ariz_US_7/SearchItem.java
+++ src/test/java/com/trycloud/tests/base/ariz_US_7/SearchItem.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -81,5 +82,11 @@ public class SearchItem {
 
 
 
+
+    }
+
+    @AfterMethod
+    public void closing(){
+        driver.close();
     }
 }
