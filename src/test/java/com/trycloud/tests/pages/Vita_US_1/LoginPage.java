5412787598ca01a59a69c8c05fddab599cb9d3ec
Vitakachun <kachunvita@gmail.com>
2021-02-09 08:36:27 -0600

    implemented POM for LoginPage

---
+++ src/test/java/com/trycloud/tests/pages/Vita_US_1/LoginPage.java
@@ -0,0 +1,27 @@
+package com.trycloud.tests.pages.Vita_US_1;
+
+import com.trycloud.tests.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    // 1. Initialize driver and object of current class by using
+    //PageFactory.initElements(driver,this)
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    // 2. We use @FindBy Annotation
+
+    @FindBy(id = "user")
+    public WebElement inputUsername;
+
+    @FindBy(id = "password")
+    public WebElement inputPassword;
+
+    @FindBy(id = "submit-form")
+    public WebElement loginButton;
+
+}
---
+++ src/test/java/com/trycloud/tests/pages/Vita_US_7/HomePage.java
@@ -0,0 +1,4 @@
+package com.trycloud.tests.pages.Vita_US_7;
+
+public class HomePage {
+}
