4773f0a322c34b690037cb2167cd54e2ec216187
Rosie-g <ruz0408@gmail.com>
2021-02-08 21:06:54 -0800

    created LoginPage in Pages package

---
+++ src/test/java/com/trycloud/tests/pages/rosie_US_3/LoginPage.java
@@ -0,0 +1,26 @@
+package com.trycloud.tests.pages.rosie_US_3;
+
+import com.trycloud.tests.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "user")
+    public WebElement username;
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(id = "submit-form")
+    public WebElement loginButton;
+
+
+
+}
