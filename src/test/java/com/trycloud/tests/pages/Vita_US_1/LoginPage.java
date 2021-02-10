342921ca605907fdb08b2386dc8b80b7599faafc
Vitakachun <kachunvita@gmail.com>
2021-02-09 18:52:58 -0600

    updated Login functionality

--- src/test/java/com/trycloud/tests/pages/Vita_US_1/LoginPage.java
+++ src/test/java/com/trycloud/tests/pages/Vita_US_1/LoginPage.java
@@ -24,4 +24,13 @@ public class LoginPage {
     @FindBy(id = "submit-form")
     public WebElement loginButton;
 
+    @FindBy(xpath = "//div[@id='expand']")
+    public WebElement settingsButton;
+
+    @FindBy(xpath = "//li[@data-id='logout']")
+    public WebElement logoutButton;
+
+    @FindBy(xpath = "//p[@class='warning wrongPasswordMsg']")
+    public WebElement warningMessage;
+
 }
