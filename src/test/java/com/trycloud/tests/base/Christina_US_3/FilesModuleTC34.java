25196195798b1682b2ec39d11372a6b3ceeaa2c5
cristinadolganiuc <dolgcris@gmail.com>
2021-02-11 17:40:19 -0600

    new stuff up

--- src/test/java/com/trycloud/tests/base/Christina_US_3/FilesModuleTC34.java
+++ src/test/java/com/trycloud/tests/base/Christina_US_3/FilesModuleTC34.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -40,17 +41,29 @@ public class FilesModuleTC34 extends TestBase{
       actionIcon.click();
 
       //Choose “Add to Favorites” option
-      WebElement addtoFavorites =driver.findElement(By.xpath("//*[@class='icon icon-starred']"));
+      WebElement addtoFavorites =driver.findElement(By.xpath("//a[@class='menuitem action action-favorite permanent']"));
+      addtoFavorites.click();
      // Select dropdown= new Select(driver.findElement(By.xpath("//*[@class='icon icon-starred']")));
       //dropdown.getFirstSelectedOption();
-      Thread.sleep(2000);
+      Thread.sleep(3000);
 
       //4. Click “Favorites” sub-module on the left side
-      WebElement favorites= driver.findElement(By.xpath("//*[@class='nav-icon-favorites svg']"));
+      //finding all locators for sub-module( or sub-menu)
+      WebElement files2= driver.findElement(By.xpath("//a[@class='active']"));
 
+      WebElement allFiles= driver.findElement(By.xpath("//a[@class='nav-icon-files svg active']"));
 
+      WebElement favorites= driver.findElement(By.xpath("//a[@class='nav-icon-favorites svg']"));
+
+      //in order to select an item from sub-menu of a menu used mouse over action
+      Actions action= new Actions(driver);
+      action.moveToElement(files2).perform();
+      action.moveToElement(allFiles).perform();
+      action.moveToElement(favorites).click().perform();
+      Thread.sleep(2000);
 
       //5. Verify the chosen file is listed on the table
+
 
         }
 
