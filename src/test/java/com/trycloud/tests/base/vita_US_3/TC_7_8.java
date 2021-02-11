1a9551d136cf1055a1efca4c4d93994b6f636784
Vitakachun <kachunvita@gmail.com>
2021-02-10 21:17:47 -0600

    updated US#3

--- src/test/java/com/trycloud/tests/base/vita_US_3/TC_7_8.java
+++ src/test/java/com/trycloud/tests/base/vita_US_3/TC_7_8.java
@@ -1,5 +1,6 @@
 package com.trycloud.tests.base.vita_US_3;
 
+import com.google.common.base.Verify;
 import com.trycloud.tests.pages.Vita_US_3.FilesPage;
 import com.trycloud.tests.pages.Vita_US_7.HomePage;
 import com.trycloud.tests.utilities.*;
@@ -10,12 +11,20 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class TC_7_8 {
 
 
     // Test case #7 - verify users can upload a file inside a folder
     @Test
-    public static void uploadFileInsideFolder() throws InterruptedException {
+    public static void uploadFileInsideFolder() throws AWTException {
 
         //Creating the object of the class to be able to reach web elements from that class
         FilesPage filesPage = new FilesPage();
@@ -37,16 +46,73 @@ public class TC_7_8 {
 
         Actions actions = new Actions(Driver.getDriver());
 
+        // 4.Click “upload file”
         filesPage.uploadFileButton.click();
+        //  5.Upload a file
         String path = "/Users/vita/Desktop/Day1/file1.txt";
-        actions.moveToElement(filesPage.uploadFileButton).sendKeys(path).perform();
-        /*
-        4.Click “upload file”
-        5.Upload a file
-        6.Verify the file is displayed on the page
-        (Pre-condition: there should be at least 1 folder is created on the filers page)
-         */
+        File file = new File(path);
+        StringSelection stringSelection = new StringSelection(file.getAbsolutePath());
+
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+
+        Robot robot = new Robot();
+
+        // Cmd + Tab is needed since it launches a Java app and the browser looses focus
+
+        try {
+            robot.keyPress(KeyEvent.VK_META);
+            robot.keyPress(KeyEvent.VK_TAB);
+            robot.keyRelease(KeyEvent.VK_META);
+            robot.keyRelease(KeyEvent.VK_TAB);
+            robot.delay(500);
+            //Open Goto window
+            robot.keyPress(KeyEvent.VK_META);
+            robot.keyPress(KeyEvent.VK_SHIFT);
+            robot.keyPress(KeyEvent.VK_G);
+            robot.keyRelease(KeyEvent.VK_META);
+            robot.keyRelease(KeyEvent.VK_SHIFT);
+            robot.keyRelease(KeyEvent.VK_G);
+            //Paste the clipboard value
+            robot.keyPress(KeyEvent.VK_META);
+            robot.keyPress(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_META);
+            robot.keyRelease(KeyEvent.VK_V);
+            //Press Enter key to close the Goto window and Upload window
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+            robot.delay(500);
+            robot.keyPress(KeyEvent.VK_S);
+            robot.keyRelease(KeyEvent.VK_S);
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+        } catch (IllegalArgumentException e) {
+            robot.setAutoDelay(3000);
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_V);
+            //Release
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            robot.keyRelease(KeyEvent.VK_V);
+            //Enter
+            robot.setAutoDelay(3000);
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+            robot.setAutoDelay(2000);
+        }
+        // 6. Verify the file is displayed on the page
+        //  (Pre-condition: there should be at least 1 folder is created on the filers page)
+
+        List<WebElement> list = new ArrayList<>();
+        list.addAll(Driver.getDriver().findElements(By.id("fileList")));
+
+
+        for (WebElement each : list) {
+            if (each.getText().contains(path)) {
+                Assert.assertEquals(each.getText(), path);
+                return;
+            }
+        }
     }
+
 
     // Test case #8a - verify users can delete a file/folder.
     @Test
